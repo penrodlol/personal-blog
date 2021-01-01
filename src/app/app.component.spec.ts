9ae8208d95a8b49d202fda9004d2383cac4a48d1
penrodlol <penrod.christian7984@gmail.com>
2021-01-01 16:45:34 -0500

    chore: remaining 'personal-blog' refs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'personal-blog'`, () => {
+  it(`should have as title 'portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('personal-blog');
+    expect(app.title).toEqual('portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('personal-blog app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('portfolio app is running!');
   });
 });
