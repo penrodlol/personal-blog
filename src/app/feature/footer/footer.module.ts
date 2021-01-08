39666e3fbf5dbd07803318bdfefe3948314f16cd
penrodlol <penrod.christian7984@gmail.com>
2021-01-08 18:53:00 -0500

    feat: footer * Initial implementation

--- src/app/feature/footer/footer.module.ts
+++ src/app/feature/footer/footer.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { SvgIconsModule } from '@ngneat/svg-icon';
 
 import { FooterComponent } from './components/footer.component';
+import { portfolioTwitter, portfolioGithub, portfolioEnvelope } from 'src/app';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,13 @@ import { FooterComponent } from './components/footer.component';
   imports: [
     CommonModule,
     FlexLayoutModule,
+    SvgIconsModule.forRoot({
+      icons: [
+        portfolioTwitter,
+        portfolioGithub,
+        portfolioEnvelope,
+      ],
+    }),
   ],
   exports: [
     FooterComponent,
