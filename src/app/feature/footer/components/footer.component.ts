20f3f6b11954fe84231a0fc466744b2cd9da1760
penrodlol <penrod.christian7984@gmail.com>
2021-01-08 23:55:57 -0500

    feat: footer * Incorrect social names. * Social name and icon not aligned vertically.

--- src/app/feature/footer/components/footer.component.ts
+++ src/app/feature/footer/components/footer.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { PORTFOLIO_ROUTES } from 'src/app/app-routing.module';
 
+export interface ISocial {
+  name: string;
+  icon: string;
+  url: string;
+}
+
 @Component({
   selector: 'portfolio-footer',
   templateUrl: './footer.component.html',
@@ -8,9 +14,9 @@ import { PORTFOLIO_ROUTES } from 'src/app/app-routing.module';
 })
 export class FooterComponent {
   readonly ROUTES = PORTFOLIO_ROUTES;
-  readonly SOCIALS: ReadonlyArray<{ icon: string, url: string }> = [
-    { icon: 'twitter', url: 'https://twitter.com/penrodlol' },
-    { icon: 'github', url: 'https://github.com/penrodlol' },
-    { icon: 'envelope', url: 'mailto:penrod.christian7984@gmail.com' },
+  readonly SOCIALS: ReadonlyArray<ISocial> = [
+    { name: 'Twitter', icon: 'twitter', url: 'https://twitter.com/penrodlol' },
+    { name: 'Github', icon: 'github', url: 'https://github.com/penrodlol' },
+    { name: 'Email', icon: 'envelope', url: 'mailto:penrod.christian7984@gmail.com' },
   ];
 }
