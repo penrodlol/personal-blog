958a2748e57d8644df435210ad92cfb91039686e
penrodlol <penrod.christian7984@gmail.com>
2021-01-18 19:01:39 -0500

    chore: GSAP CSSPlugin registration (needed for prod bundle)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { gsap } from 'gsap';
+import { CSSPlugin } from 'gsap/CSSPlugin';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
+
+gsap.registerPlugin(CSSPlugin);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
