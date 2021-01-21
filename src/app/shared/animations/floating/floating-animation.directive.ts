12ea5a17656f7716810553f02c06e34a582d9938
penrodlol <penrod.christian7984@gmail.com>
2021-01-20 22:47:17 -0500

    feat: floating animation * More fluid motion (still needs some work but is pretty good as it stands).

--- src/app/shared/animations/floating/floating-animation.directive.ts
+++ src/app/shared/animations/floating/floating-animation.directive.ts
@@ -8,7 +8,8 @@ import { Sine, TimelineMax } from 'gsap';
 
 @Directive({ selector: '[portfolioFloatingAnimation]' })
 export class FloatingAnimationDirective implements OnDestroy {
-  timeline = new TimelineMax({
+  private readonly DURATION = 5;
+  private timeline = new TimelineMax({
     paused: true,
     repeat: -1,
     yoyo: true,
@@ -16,16 +17,22 @@ export class FloatingAnimationDirective implements OnDestroy {
 
   constructor({ nativeElement }: ElementRef) {
     this.timeline
-      .set(nativeElement, {
-        x: 0,
-        y: 0,
-        rotation: 0,
-        ease: Sine.easeInOut,
-      })
-      .to(nativeElement, 5, {
+      .to(nativeElement, this.DURATION, {
         x: 5,
         y: -5,
         rotation: 5,
+        ease: Sine.easeInOut,
+      })
+      .to(nativeElement, this.DURATION, {
+        x: -5,
+        y: -10,
+        rotation: 3,
+        ease: Sine.easeInOut,
+      })
+      .to(nativeElement, this.DURATION, {
+        x: 0,
+        y: 0,
+        rotation: 0,
         ease: Sine.easeInOut,
       })
       .play();
